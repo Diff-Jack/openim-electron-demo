c99b7f51b36d113b59550927d16ecacac26d88bc
Diff-Jack <hongjie@tensoropera.com>
2025-06-19 11:07:23 +0800

    modify friends request style

--- src/components/ApplicationItem/index.tsx
+++ src/components/ApplicationItem/index.tsx
@@ -7,8 +7,9 @@ import { Button, Spin } from "antd";
 import { t } from "i18next";
 import React, { memo, useCallback, useState } from "react";
 
+import agree from "@/assets/images/contact/agree.png";
 import arrow from "@/assets/images/contact/arrowTopRight.png";
-import checked from "@/assets/images/contact/checked.png";
+import refuse from "@/assets/images/contact/refuse.png";
 import OIMAvatar from "@/components/OIMAvatar";
 import { IMSDK } from "@/layout/MainContentWrap";
 import { emit } from "@/utils/events";
@@ -85,7 +86,7 @@ const ApplicationItem = ({
   return (
     <Spin spinning={loading}>
       <div className="mb-2 flex cursor-pointer flex-row items-center justify-between bg-gradient-to-r from-[rgba(255,255,255,0.39)] from-0% to-[rgba(230,230,230,0.1521)] to-100% py-[22px] pl-8 pr-12 transition-colors hover:bg-[#F3F3F3]">
-        <div className="flex flex-row">
+        <div className="mr-[138px] flex flex-row">
           <OIMAvatar
             src={getAvatarUrl()}
             text={getTitle()}
@@ -102,8 +103,9 @@ const ApplicationItem = ({
                 </span>
               )}
             </p>
-            <p className="text-xs text-[#737373]">{t("application.information")}:</p>
-            <p className="text-xs text-[#737373]">{source.reqMsg}</p>
+            <p className="line-clamp-4 text-xs text-[#737373]">
+              {t("application.information")}: {source.reqMsg}
+            </p>
           </div>
         </div>
 
@@ -146,16 +148,18 @@ const ApplicationItem = ({
 
 export default memo(ApplicationItem);
 
+const STATUS_ICON_MAP: Partial<Record<ApplicationHandleResult, string>> = {
+  [ApplicationHandleResult.Agree]: agree,
+  [ApplicationHandleResult.Reject]: refuse,
+};
+
 const StatusIcon: React.FC<{
   isRecv: boolean;
   status?: ApplicationHandleResult;
 }> = ({ isRecv, status }) => {
   if (isRecv) return null;
 
-  switch (status) {
-    case ApplicationHandleResult.Agree:
-      return <img className="mr-2 h-4 w-4" src={checked} alt="" />;
-    default:
-      return <img className="mr-2 h-4 w-4" src={arrow} alt="" />;
-  }
+  const iconSrc = STATUS_ICON_MAP[status!] || arrow;
+
+  return <img className="mr-2 h-4 w-4" src={iconSrc} alt="" />;
 };
